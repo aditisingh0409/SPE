c2a84131780613182ce7219284e8c20c75d209ef
Aditi Singh <64026259+aditisingh0409@users.noreply.github.com>
2024-02-13 19:42:34 +0530

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python3
+# This Python program will print Hello World...
+print("Hello World Demo...Jan 2024...\n")
+print("Hello World Demo...Jan 2024...\n")
+print("Hello World Demo...Jan 2024...\n")
+print("Hello World Demo...Jan 2024...\n")
