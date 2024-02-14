1ce0087136d864f7559f7c06e7a4b9eddc475a7c
Aditi Singh <64026259+aditisingh0409@users.noreply.github.com>
2024-02-14 19:20:19 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,5 +2,3 @@
 # This Python program will print Hello World...
 print("Hello World Demo...Feb 2024...\n")
 print("Hello World Demo...Feb 2024...\n")
-print("Hello World Demo...Feb 2024...\n")
-print("Hello World Demo...Feb 2024...\n")
